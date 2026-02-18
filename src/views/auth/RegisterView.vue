ea4bc6eda9f85e54e57d3590867fdbcbf17d4591
reyvanomario <reyvanomario24122004@gmail.com>
2026-02-18 15:47:49 +0700

    fix logo

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -27,9 +27,6 @@ const handleRegister = async () => {
 <template>
   <section class="bg-gray-50 mt-6">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-          TravelAPAP    
-      </a>
       <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
